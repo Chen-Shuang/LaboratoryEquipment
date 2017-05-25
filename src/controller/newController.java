20ffefb93e07ad6fa67316c8230d00d5051fd9f7
Chen-Shuang <chen_s_vip@163.com>
2017-05-26 00:57:28 +0800

    完成除上传Excel导入数据的其他功能

--- src/controller/newController.java
+++ src/controller/newController.java
@@ -1,14 +1,221 @@
 package controller;
 
+import java.io.File;
+import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import model.ItemsModel;
+import model.newItemsModel;
+import model.repairItemsModel;
+import model.scrapItemsModel;
+
 import com.jfinal.core.Controller;
+import com.jfinal.kit.PathKit;
+import com.jfinal.plugin.activerecord.Db;
+import com.jfinal.plugin.activerecord.IAtom;
+import com.jfinal.plugin.activerecord.Page;
 /**
  * 新添设备
- * @author 陈爽 & 
+ * @author 陈爽 
  *
  */
 public class newController extends Controller {
 
 	public void index() {
 		render("new.html");
+	}
+	
+	/**
+	 * 获取新添设备信息
+	 */
+	public void getNewItemsInfo() {
+		int curr = getParaToInt("curr"); // 获取页码
+		int size = getParaToInt("size"); // 获取页面显示数据
+		String search = getPara("search"); // 获取搜索条件
+		String sTime = getPara("sTime"); // 获取起始时间  日期搜索是查询的
+		String eTime = getPara("eTime"); // 获取终止时间
+		
+		// 判断是否选择了时间,时间格式转换
+		if(sTime==""){
+			sTime = "1900-01-01";
+		}
+		if(eTime==""){
+			eTime += "2099-06-16"; // 当前系统时间
+		}
+		Page<newItemsModel> newItems = newItemsModel.dao.getNewItemsInfo(curr, size, search,sTime,eTime); // 查询需购设备信息
+		renderJson(newItems);
+	}
+	
+	/**
+	 * 添加需购设备
+	 */
+	public void addNewItem() {
+		final String userLoginId = getSession().getAttribute("userLoginId").toString(); // 获取登录人id
+		
+		final ItemsModel items = getModel(ItemsModel.class, "items"); // 获取序列化表单数据
+		final newItemsModel newItems = getModel(newItemsModel.class, "new_items"); // 获取序列化表单数据
+		
+		SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");//设置日期格式
+		final String nowTime = df.format(new Date()).toString(); // 获取当前系统时间
+		
+		final String code = "A123"; // 获取该信息的编码（自动编号）
+		
+		boolean success = Db.tx(new IAtom() {  // 添加事务处理
+	        @Override
+	        public boolean run() throws SQLException {
+	            try {
+	            	items.set("code", code).set("createTime", nowTime).set("updateTime", nowTime)
+	            		.set("status", 1).set("user_login_id", userLoginId).save();  // 保存新添设备的基本信息
+	        		newItems.set("items_id", items.getLong("id")).set("status", 0).save(); // 保存新添设备信息{items.getLong("id") 获取保存后对象的id}
+	            } catch (Exception e) {
+	                e.printStackTrace();
+	                return false;
+	            }
+	            return true;
+	        }
+	    });
+	    
+	    if(success){
+	    	renderText("success");
+	    }else{
+	    	renderText("error");
+	    }
+	}
+	
+	/**
+	 * 获取单个设备信息
+	 */
+	public void getNewItemInfo() {
+		int itemsId = getParaToInt("itemsId"); // 获取设备id
+		newItemsModel newItem = newItemsModel.dao.getNewItemInfo(itemsId); // 获取设备信息
+		renderJson(newItem);
+	}
+	
+	/**
+	 * 修改设备信息
+	 */
+	public void updateItemInfo() {
+		final ItemsModel items = getModel(ItemsModel.class, "items"); // 获取序列化表单数据
+		final newItemsModel newItems = getModel(newItemsModel.class, "new_items"); // 获取序列化表单数据
+		
+		SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");//设置日期格式
+		final String nowTime = df.format(new Date()).toString(); // 获取当前系统时间
+		
+		boolean success = Db.tx(new IAtom() {  // 添加事务处理
+	        @Override
+	        public boolean run() throws SQLException {
+	            try {
+	            	items.set("updateTime", nowTime).update();  // 修改新添设备的基本信息
+	        		newItems.update(); // 修改新添设备信息
+	            } catch (Exception e) {
+	                e.printStackTrace();
+	                return false;
+	            }
+	            return true;
+	        }
+	    });
+	    
+	    if(success){
+	    	renderText("success");
+	    }else{
+	    	renderText("error");
+	    }
+	}
+	
+	/**
+	 * 删除单个设备信息
+	 */
+	public void delItemInfo() {
+		int itemsId = getParaToInt("itemsId"); // 获取设备id
+		int count = ItemsModel.dao.delItemInfo(itemsId);
+		if(count>0){
+			renderText("success");
+		}else{
+			renderText("error");
+		}
+		
+	}
+	
+	/**
+	 * 设备状态设置为维修
+	 */
+	public void toRepairItem(){
+		final ItemsModel items = getModel(ItemsModel.class, "items"); // 获取设备信息的id
+		final newItemsModel newItems = getModel(newItemsModel.class, "new_items"); // 获取新添设备信息的id
+		final repairItemsModel repairItems = getModel(repairItemsModel.class, "repair_items"); // 获取修改设备信息的序列化表单数据
+		
+		SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");//设置日期格式
+		final String nowTime = df.format(new Date()).toString(); // 获取当前系统时间
+		
+		boolean success = Db.tx(new IAtom() {  // 添加事务处理
+	        @Override
+	        public boolean run() throws SQLException {
+	            try {
+	            	items.set("status", 2).set("updateTime", nowTime).update();  // 将设备状态设置为待维修状态
+	        		newItems.set("status", -1).update(); // 将新添状态设置为状态更改
+	        		repairItems.set("items_id", items.getLong("id")).set("status", 0).save(); // 插入该条设备修改的信息，状态为待修改
+	            } catch (Exception e) {
+	                e.printStackTrace();
+	                return false;
+	            }
+	            return true;
+	        }
+	    });
+	    
+	    if(success){
+	    	renderText("success");
+	    }else{
+	    	renderText("error");
+	    }
+	}
+	
+	/**
+	 * 设备状态设置为报废
+	 */
+	public void toScrapItem(){
+		final ItemsModel items = getModel(ItemsModel.class, "items"); // 获取设备信息的id
+		final newItemsModel newItems = getModel(newItemsModel.class, "new_items"); // 获取新添设备信息的id
+		final scrapItemsModel scrapItems = getModel(scrapItemsModel.class, "scrap_items"); // 获取修改设备信息的序列化表单数据
+		
+		SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");//设置日期格式
+		final String nowTime = df.format(new Date()).toString(); // 获取当前系统时间
+		
+		boolean success = Db.tx(new IAtom() {  // 添加事务处理
+	        @Override
+	        public boolean run() throws SQLException {
+	            try {
+	            	items.set("status", 3).set("updateTime", nowTime).update();  // 将设备状态设置为待报废状态
+	        		newItems.set("status", -1).update(); // 将新添状态设置为状态已更改
+	        		scrapItems.set("items_id", items.getLong("id")).set("status", 0).save(); // 插入该条设备修改的信息，状态为待审核
+	            } catch (Exception e) {
+	                e.printStackTrace();
+	                return false;
+	            }
+	            return true;
+	        }
+	    });
+	    
+	    if(success){
+	    	renderText("success");
+	    }else{
+	    	renderText("error");
+	    }
+	}
+	
+	/**
+	 * Excel模板下载
+	 */
+	public void downloadTemplate() {
+		int type = getParaToInt("type"); // 获取要下载的文件类型 （0：说明文档；3：2003版本；7:2007版本）
+		String realFile = null;
+		if(type==3){
+			realFile = PathKit.getWebRootPath() + "/template/" + "新添设备信息录入模板.xls"; // 根据路径获取文件-2003版
+		}else if(type==7){
+			realFile = PathKit.getWebRootPath() + "/template/" + "新添设备信息录入模板.xlsx"; // 根据路径获取文件-2007版
+		}else{
+			realFile = PathKit.getWebRootPath() + "/template/" + "批量文件上传使用说明文档.txt"; // 根据路径获取文件
+		}
+		renderFile(new File(realFile));
 	}
 }
--- src/model/newItemsModel.java
+++ src/model/newItemsModel.java
@@ -1,12 +1,38 @@
 package model;
 
 import com.jfinal.plugin.activerecord.Model;
+import com.jfinal.plugin.activerecord.Page;
 /**
  * 新添设备
- * @author 陈爽 & 
+ * @author 陈爽 
  *
  */
 public class newItemsModel extends Model<newItemsModel> {
 
 	public static final newItemsModel dao= new newItemsModel();
+	
+	/**
+	 * 按分页获取设备信息
+	 * @param curr 当前页
+	 * @param size 页面显示数量
+	 * @param search 查询内容
+	 * @param sTime 起始时间
+	 * @param eTime 结束时间
+	 * @return 返回该页设备信息
+	 */
+	public Page<newItemsModel> getNewItemsInfo(int curr, int size, String search, String sTime, String eTime){
+		return paginate(curr, size, "select a.*,b.*,a.id items_id,b.status new_status",
+			" from items a,new_items b where a.status=1 and b.status=0 and a.id=b.items_id and concat(a.code,a.name,a.type) like '%"+search+"%' "
+					+ "and b.purchaseDate >= '"+sTime+"' and b.purchaseDate <= '"+eTime+"' order by a.createTime desc");
+	}
+	
+	/**
+	 * 获取单个设备信息
+	 * @param itemsId 设备id
+	 * @return 返回查询的设备对象
+	 */
+	public newItemsModel getNewItemInfo(int itemsId) {
+		return newItemsModel.dao.findFirst("select a.*,b.*,a.id items_id,b.id new_id"
+				+ "  from items a,new_items b where a.status=1 and b.status=0 and a.id=b.items_id and a.id=?",itemsId);
+	}
 }
