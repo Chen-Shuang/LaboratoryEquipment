6a1d8f03e1986c25c33907e90bc35b9e88075261
Chen-Shuang <chen_s_vip@163.com>
2017-05-29 01:59:06 +0800

    完成报废页面功能

--- src/controller/scrapController.java
+++ src/controller/scrapController.java
@@ -3,7 +3,18 @@
  */
 package controller;
 
+import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import model.ItemsModel;
+import model.repairItemsModel;
+import model.scrapItemsModel;
+
 import com.jfinal.core.Controller;
+import com.jfinal.plugin.activerecord.Db;
+import com.jfinal.plugin.activerecord.IAtom;
+import com.jfinal.plugin.activerecord.Page;
 
 /**
  * 报废设备
@@ -14,5 +25,81 @@ public class scrapController extends Controller {
 
 	public void index() {
 		render("scrap.html");
+	}
+	
+	/**
+	 * 获取维修的设备信息
+	 */
+	public void getScrapItemsInfo() {
+		int curr = getParaToInt("curr"); // 获取页码
+		int size = getParaToInt("size"); // 获取页面显示数据
+		String search = getPara("search"); // 获取搜索条件
+		String sTime = getPara("sTime"); // 获取起始时间  日期搜索是查询的
+		String eTime = getPara("eTime"); // 获取终止时间
+		int status = getParaToInt("status"); // 获取维修状态
+		
+		// 判断是否选择了时间,时间格式转换
+		if(sTime==""){
+			sTime = "1900-01-01";
+		}
+		if(eTime==""){
+			eTime += "2099-06-16"; // 当前系统时间
+		}
+		Page<scrapItemsModel> scrapItems = scrapItemsModel.dao.getScrapItemsInfo(curr, size, search,sTime,eTime,status); // 查询维修设备信息
+		renderJson(scrapItems);
+	}
+	
+	/**
+	 * 获取单个设备信息
+	 */
+	public void getScrapItemInfo() {
+		int itemsId = getParaToInt("itemsId"); // 获取设备id
+		scrapItemsModel scrapItem = scrapItemsModel.dao.getScrapItemInfo(itemsId); // 获取设备信息
+		renderJson(scrapItem);
+	}
+	
+	/**
+	 * 修改报废设备信息
+	 */
+	public void updateItemInfo() {
+		final ItemsModel items = getModel(ItemsModel.class, "items"); // 获取序列化表单数据
+		final scrapItemsModel scrapItems = getModel(scrapItemsModel.class, "scrap_items"); // 获取序列化表单数据
+		
+		SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");//设置日期格式
+		final String nowTime = df.format(new Date()).toString(); // 获取当前系统时间
+		
+		boolean success = Db.tx(new IAtom() {  // 添加事务处理
+	        @Override
+	        public boolean run() throws SQLException {
+	            try {
+	            	items.set("updateTime", nowTime).update();  // 修改设备的基本信息
+	            	scrapItems.update(); // 修改报废设备信息
+	            } catch (Exception e) {
+	                e.printStackTrace();
+	                return false;
+	            }
+	            return true;
+	        }
+	    });
+	    
+	    if(success){
+	    	renderText("success");
+	    }else{
+	    	renderText("error");
+	    }
+	}
+	
+	/**
+	 * 删除单个设备信息
+	 */
+	public void delItemInfo() {
+		int itemsId = getParaToInt("itemsId"); // 获取设备id
+		int count = ItemsModel.dao.delItemInfo(itemsId);
+		if(count>0){
+			renderText("success");
+		}else{
+			renderText("error");
+		}
+		
 	}
 }
--- src/model/scrapItemsModel.java
+++ src/model/scrapItemsModel.java
@@ -1,6 +1,7 @@
 package model;
 
 import com.jfinal.plugin.activerecord.Model;
+import com.jfinal.plugin.activerecord.Page;
 /**
  * 报废设备
  * @author 陈爽 & 
@@ -9,4 +10,30 @@ import com.jfinal.plugin.activerecord.Model;
 public class scrapItemsModel extends Model<scrapItemsModel> {
 
 	public static final scrapItemsModel dao= new scrapItemsModel();
+	
+	/**
+	 * 按分页获取报废的设备信息
+	 * @param curr 当前页
+	 * @param size 页面显示数量
+	 * @param search 查询内容
+	 * @param sTime 起始时间
+	 * @param eTime 结束时间
+	 * @return 返回该页设备信息
+	 */
+	public Page<scrapItemsModel> getScrapItemsInfo(int curr, int size, String search, String sTime, String eTime, int status){
+		return paginate(curr, size, "select a.*,b.*,a.id items_id,b.status scrap_status",
+				" from items a,scrap_items b where a.status=3 and b.status='"+status+"' and a.id=b.items_id and concat(a.code,a.name,a.type) like '%"+search+"%' "
+						+ "and b.scrapDate >= '"+sTime+"' and b.scrapDate <= '"+eTime+"' order by a.createTime desc");
+		
+	}
+	
+	/**
+	 * 获取单个设备信息
+	 * @param itemsId 设备id
+	 * @return 返回查询的设备对象
+	 */
+	public scrapItemsModel getScrapItemInfo(int itemsId) {
+		return scrapItemsModel.dao.findFirst("select a.*,b.*,a.id items_id,b.id scrap_id"
+				+ "  from items a,scrap_items b where a.id=b.items_id and a.id=?",itemsId);
+	}
 }
