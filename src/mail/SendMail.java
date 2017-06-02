ea78d6b079d53f9b787aae4ed3a95e98067519d2
Chen-Shuang <chen_s_vip@163.com>
2017-06-02 21:11:47 +0800

    删除多余代码

--- src/mail/SendMail.java
+++ src/mail/SendMail.java
@@ -1,7 +1,6 @@
 package mail;
 
 import java.io.UnsupportedEncodingException;
-import java.security.GeneralSecurityException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
