cbfbbb19c03a9a264c35f8d923d422db5d5d4391
Chen-Shuang <chen_s_vip@163.com>
2017-06-06 17:12:51 +0800

    修改页面传值丢失问题

--- src/interceptor/LoginInterceptor.java
+++ src/interceptor/LoginInterceptor.java
@@ -19,10 +19,12 @@ public class LoginInterceptor implements Interceptor {
 	   	Controller controller = arg0.getController(); // 获取用户正在请求的Controller
 	   	Object loginUser = controller.getSession().getAttribute("userLoginId");  // 获取用户登录的session
 	   	
-	    if (loginUser != null) 
+	    if (loginUser != null){
 	    	arg0.invoke(); // 如果session不为空，则放行
-	    else
-	        controller.setAttr("webUrl",new MainConfig().webUrl); // 返回项目地址
-	        controller.redirect("/login.html"); // 负责跳转到登录页面
+	    }else{
+	    	 String webUrl = new MainConfig().webUrl; // 获取项目地址
+	    	 controller.setAttr("webUrl",webUrl); // 返回项目地址
+		     controller.render("/login.html"); // 负责跳转到登录页面
+	    }
 	}
 }
